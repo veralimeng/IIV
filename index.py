205a9efb842d9f341c8e5786df64b03eddba3c0d
veralimeng <veralimeng@hotmail.com>
2024-06-13 22:08:29 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,15 @@
+import requests
+
+def handler(event, context):
+    url = "https://raw.githubusercontent.com/Vera-lm/IIV/main/code.py"
+    
+    response = requests.get(url)
+    with open("/tmp/code.py", "w") as f:
+        f.write(response.text)
+    
+    exec(open("/tmp/code.py").read())
+    
+    return {
+        "statusCode": 200,
+        "body": "code.py executed successfully"
+    }
