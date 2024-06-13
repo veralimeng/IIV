25e20fa30ea0bd442b3339d3350c56f185cf9813
veralimeng <veralimeng@hotmail.com>
2024-06-13 22:43:01 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,55 @@
+import os
 import requests
+import zipfile
 
 def handler(event, context):
-    url = "https://raw.githubusercontent.com/veralimeng/IIV/main/code.py"
-    
+    zip_url = "https://github.com/veralimeng/IIV/archive/refs/heads/main.zip"
+    zip_path = "/tmp/main.zip"
+    extract_path = "/tmp/main"
+
     try:
-        response = requests.get(url)
+        # 下载 ZIP 文件
+        response = requests.get(zip_url)
         response.raise_for_status()
-        with open("/tmp/code.py", "w") as f:
-            f.write(response.text)
-        
-        exec(open("/tmp/code.py").read())
+        with open(zip_path, "wb") as f:
+            f.write(response.content)
+        print("ZIP file downloaded successfully.")
+
+        # 解压 ZIP 文件
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.extractall(extract_path)
+        print("ZIP file extracted successfully.")
+
+        # 确定解压后文件路径
+        code_file_path = os.path.join(extract_path, "IIV-main", "code.py")
+        print(f"Code file path: {code_file_path}")
+
+        # 执行解压后的 code.py 文件
+        exec(open(code_file_path).read())
+        print("Code file executed successfully.")
         
         return {
             "statusCode": 200,
-            "body": "code.py executed successfully"
+            "body": "code.py executed and run successfully"
         }
     except requests.exceptions.RequestException as e:
+        error_message = f"Failed to download code.py: {e}"
+        print(error_message)
         return {
             "statusCode": 500,
-            "body": f"Failed to download or execute code.py: {e}"
+            "body": error_message
+        }
+    except zipfile.BadZipFile as e:
+        error_message = f"Failed to unzip the file: {e}"
+        print(error_message)
+        return {
+            "statusCode": 500,
+            "body": error_message
+        }
+    except Exception as e:
+        error_message = f"An error occurred: {e}"
+        print(error_message)
+        return {
+            "statusCode": 500,
+            "body": error_message
         }
