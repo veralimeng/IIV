a6c9b701b65993ebdf5214d847b0e89e14a961c9
veralimeng <veralimeng@hotmail.com>
2024-06-14 12:24:10 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,10 +3,10 @@ import os
 
 def handler(event, context):
     # 下载 code.py 文件
-    code_url = "https://raw.githubusercontent.com/Vera-lm/IIV/main/code.py"
-    data_url1 = "https://raw.githubusercontent.com/Vera-lm/IIV/main/crunchbase_export-4-10-2019.xlsx"
-    data_url2 = "https://raw.githubusercontent.com/Vera-lm/IIV/main/combined_tweet_df.csv"
-    data_url3 = "https://raw.githubusercontent.com/Vera-lm/IIV/main/user.csv"
+    code_url = "https://raw.githubusercontent.com/veralimeng/IIV/main/code.py"
+    # data_url1 = "https://raw.githubusercontent.com/Vera-lm/IIV/main/crunchbase_export-4-10-2019.xlsx"
+    # data_url2 = "https://raw.githubusercontent.com/Vera-lm/IIV/main/combined_tweet_df.csv"
+    # data_url3 = "https://raw.githubusercontent.com/Vera-lm/IIV/main/user.csv"
 
     try:
         # 下载 code.py 文件
@@ -16,24 +16,24 @@ def handler(event, context):
         with open(code_path, "w") as f:
             f.write(code_response.text)
 
-        # 下载数据文件
-        data_response1 = requests.get(data_url1, timeout=10)
-        data_response1.raise_for_status()
-        data_path1 = "/tmp/crunchbase_export-4-10-2019.xlsx"
-        with open(data_path1, "wb") as f:
-            f.write(data_response1.content)
+        # # 下载数据文件
+        # data_response1 = requests.get(data_url1, timeout=10)
+        # data_response1.raise_for_status()
+        # data_path1 = "/tmp/crunchbase_export-4-10-2019.xlsx"
+        # with open(data_path1, "wb") as f:
+        #     f.write(data_response1.content)
         
-        data_response2 = requests.get(data_url2, timeout=10)
-        data_response2.raise_for_status()
-        data_path2 = "/tmp/combined_tweet_df.csv"
-        with open(data_path2, "wb") as f:
-            f.write(data_response2.content)
+        # data_response2 = requests.get(data_url2, timeout=10)
+        # data_response2.raise_for_status()
+        # data_path2 = "/tmp/combined_tweet_df.csv"
+        # with open(data_path2, "wb") as f:
+        #     f.write(data_response2.content)
         
-        data_response3 = requests.get(data_url3, timeout=10)
-        data_response3.raise_for_status()
-        data_path3 = "/tmp/user.csv"
-        with open(data_path3, "wb") as f:
-            f.write(data_response3.content)
+        # data_response3 = requests.get(data_url3, timeout=10)
+        # data_response3.raise_for_status()
+        # data_path3 = "/tmp/user.csv"
+        # with open(data_path3, "wb") as f:
+        #     f.write(data_response3.content)
 
         # 打印 code.py 的内容进行调试
         with open(code_path, "r") as f:
