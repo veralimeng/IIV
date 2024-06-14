2e2cfa949de9cf3fa56500db1bea3b11c6e5e72e
veralimeng <veralimeng@hotmail.com>
2024-06-14 15:42:19 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,3 @@
-pip install requests
-pip install pandas 
-pip install numpy
-pip install networkx
-pip install matplotlib
-pip install scikit-learn
-pip install pyarrow
-pip install statsmodels
-pip install linearmodels
-pip install tqdm
 
 
 import pandas as pd
