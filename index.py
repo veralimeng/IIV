ba1f960cf9ff463b2b68f993b3b8deb4e1b1e894
veralimeng <veralimeng@hotmail.com>
2024-06-14 13:37:39 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,26 +3,3 @@ import zipfile
 import requests
 
 print("开始加载数据...")
-
-# 假设 zip_file_path 是你的ZIP文件的路径
-zip_file_path = '/tmp/funding_twitter_df_copy2.csv.zip'
-csv_file_name = 'funding_twitter_df_copy2.csv'
-
-# 打开ZIP文件
-with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-    # 解压特定的CSV文件到内存
-    with zip_ref.open(csv_file_name) as csv_file:
-        # 使用Pandas读取CSV文件
-        funding_twitter_df_copy2 = pd.read_csv(csv_file)
-        
-print("数据加载完成")
-
-def ols_reg(funding_twitter_iiv_avg_df):
-    print("执行 ols_reg 函数")
-    return None
-
-def iv_2sls_reg(funding_twitter_iiv_avg_df, IIV='IIV1_TE'):
-    print("执行 iv_2sls_reg 函数")
-    return None, None, None, None
-
-print("测试执行成功")
